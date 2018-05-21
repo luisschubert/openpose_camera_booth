0b1ad99d1075b363720b1249ea7b803da67e3d6a
Luis Schubert <schubert.luis@gmail.com>
2018-05-20 19:19:14 -0700

    with comment

--- server/uiServer.js
+++ server/uiServer.js
@@ -5,62 +5,62 @@ app.use('/gallery',express.static(__dirname+'gallery'));
 app.use('/savePhoto',express.static(__dirname+'savePhoto'));
 
 app.set('port', 7777);
-app.get('/photoCaptureInformation', (req, res) => {
-    fs.readFile('photoCaptureInformation.html', (err, data) => {
-        if(err) {
-            res.statusCode = 500;
-        }
-        else {
-            res.setHeader('Content-type','text/html');
-            res.end(data);
-        }
-    })
-});
+//app.get('/photoCaptureInformation', (req, res) => {
+//    fs.readFile('photoCaptureInformation.html', (err, data) => {
+//        if(err) {
+//            res.statusCode = 500;
+//        }
+//        else {
+//            res.setHeader('Content-type','text/html');
+//            res.end(data);
+//        }
+//    })
+//});
 
-app.get('/photoCaptureCountdown', (req, res) => {
-    fs.readFile('photoCaptureCountdown.html', (err, data) => {
-        if(err) {
-            res.statusCode = 500;
-        }
-        else {
-            res.setHeader('Content-type','text/html');
-            res.end(data);
-        }
-    })
-});
-app.get('/savePhoto', (req, res) => {
-    fs.readFile('savePhoto.html', (err, data) => {
-        if(err) {
-            res.statusCode = 500;
-        }
-        else {
-            res.setHeader('Content-type','text/html');
-            res.end(data);
-        }
-    })
-});
-app.get('/photoGalleryInformation', (req, res) => {
-    fs.readFile('photoGalleryInformation.html', (err, data) => {
-        if(err) {
-            res.statusCode = 500;
-        }
-        else {
-            res.setHeader('Content-type','text/html');
-            res.end(data);
-        }
-    })
-});
-app.get('/photoGallery', (req, res) => {
-    fs.readFile('photoGallery.html', (err, data) => {
-        if(err) {
-            res.statusCode = 500;
-        }
-        else {
-            res.setHeader('Content-type','text/html');
-            res.end(data);
-        }
-    })
-});
+//app.get('/photoCaptureCountdown', (req, res) => {
+//    fs.readFile('photoCaptureCountdown.html', (err, data) => {
+//        if(err) {
+//            res.statusCode = 500;
+//        }
+//        else {
+//            res.setHeader('Content-type','text/html');
+//            res.end(data);
+//        }
+//    })
+//});
+//app.get('/savePhoto', (req, res) => {
+//    fs.readFile('savePhoto.html', (err, data) => {
+//        if(err) {
+//            res.statusCode = 500;
+//        }
+//        else {
+//            res.setHeader('Content-type','text/html');
+//            res.end(data);
+//        }
+//    })
+//});
+//app.get('/photoGalleryInformation', (req, res) => {
+//    fs.readFile('photoGalleryInformation.html', (err, data) => {
+//        if(err) {
+//            res.statusCode = 500;
+//        }
+//        else {
+//            res.setHeader('Content-type','text/html');
+//            res.end(data);
+//        }
+//    })
+//});
+//app.get('/photoGallery', (req, res) => {
+//    fs.readFile('photoGallery.html', (err, data) => {
+//        if(err) {
+//            res.statusCode = 500;
+//        }
+//        else {
+//            res.setHeader('Content-type','text/html');
+//            res.end(data);
+//        }
+//    })
+//});
 app.listen(app.get('port'), () => {
     console.log(`Node.js server is running`);
     console.log(`Listening for clients on port ${app.get('port')}`);
